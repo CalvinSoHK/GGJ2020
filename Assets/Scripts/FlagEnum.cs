81d6a7a32dbed600f04a526eab1ab10fed7ef161
Calvin So <ccs403@nyu.edu>
2020-02-01 12:37:12 -0600

    Created a StateSystem that will allow us to set flags to true and what not.

---
+++ Assets/Scripts/FlagEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ2020.Utility
+{
+    /// <summary>
+    /// Contains all enums for our flags.
+    /// Add more enums for new flags in the future.
+    /// </summary>
+    public enum FlagEnum
+    {
+        IsMean,
+        IsNice,
+        IsLoving,
+        Null
+    }
+}
---
+++ Assets/Scripts/ResourceCache.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Loads all scriptable objects as references
+/// </summary>
+public class ResourceCache : Singleton<ResourceCache>
+{
+    protected ResourceCache(){}
+
+    [SerializeField]
+    public StateSystem stateSystem;
+
+    void Start(){
+        stateSystem.InitDict();
+    }
+
+}
