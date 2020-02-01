188e2360d1ad327c35bf5af26428a9932e84b41a
Calvin So <ccs403@nyu.edu>
2020-02-01 11:30:49 -0600

    Nothing important, barely wrote anything

---
+++ Assets/Scripts/StateSystem/StateSystemSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(filename = "Data", menuName = "StateSystem")]
+public class StateSystemSO : ScriptableObject
+{
+    
+}
