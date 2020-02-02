0bbee12939fd2a4c0e99d7a99460a356c726c8be
Calvin So <ccs403@nyu.edu>
2020-02-01 21:33:23 -0600

    Added some more functionality to the UI Began work on some robot stuff.. Added an interface called IPatient that all robot script should use The RobotOne script is an exmaple.

---
+++ Assets/MapEnumToTransparency.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+///<summary> 
+///maps enum value to color
+///<summary>
+[RequireComponent(typeof(PullValue))]
+public class MapEnumToTransparency : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+
+    [SerializeField]
+    PullValue pull;
+
+    Color originalColor;
+
+    void Start(){
+        originalColor = image.color;
+    }
+
+    void Update(){
+        Color newColor = originalColor;
+        newColor.a = pull.value/100f;
+        image.color = newColor;
+    }
+}
