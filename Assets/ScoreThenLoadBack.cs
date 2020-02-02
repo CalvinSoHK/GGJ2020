bf369f8d1245076d1c9720f397f1e3a2b870beb2
Calvin So <ccs403@nyu.edu>
2020-02-02 12:28:00 -0600

    Added some loading screens and stuff Added a score screen that loads back to menu NOTE: LOADING BACK DOES NOT ALLOW YOU TO RUN THROUGH AGAIN! SOME ERROR WITH SYSTEM CACHE. CHECK LATER.

---
+++ Assets/ScoreThenLoadBack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreThenLoadBack : MonoBehaviour
+{
+    GameStateSystem gameStateSystem;
+    StateSystem stateSystem;
+    int score = 0;
+
+    public float wait_time = 3f;
+    private float timer;
+
+    public Animator animator;
+
+    void Start()
+    {
+        gameStateSystem = SystemCache.Instance.gameStateSystem;
+        stateSystem = SystemCache.Instance.stateSystem;
+
+        score = ScorePlayer();
+        timer = wait_time;
+    }
+
+    public int ScorePlayer(){
+        int score = 0;
+        if(stateSystem.IsFlag(GGJ2020.Utility.FlagEnum.Checkpoint2)){
+            score++;
+        }
+        if(stateSystem.IsFlag(GGJ2020.Utility.FlagEnum.Checkpoint3)){
+            score++;
+        }
+        return score;
+    }
+
+    void Update(){
+        if(timer <= 0){
+            if(score == 0){
+                animator.SetTrigger("BadScore");
+            }
+            else if(score == 1){
+                animator.SetTrigger("NeutralScore");
+            }
+            else{
+                animator.SetTrigger("GoodScore");
+            }
+        }
+        else{
+            timer -= Time.deltaTime;
+        }
+    }
+
+    public void LoadBack(){
+        //gameStateSystem.ResetDict();
+        //stateSystem.ResetDict();
+        SceneManager.LoadScene(0);
+    }
+}
